146eef0d00cd42dd19aa2b99edbff1de13d6c7e4
zuhalcode <zuhalcode@gmail.com>
2023-04-13 13:27:23 +0700

    make products manage, and fixing pages

--- resources/views/dashboard/branches.blade.php
+++ resources/views/dashboard/branches.blade.php
@@ -7,105 +7,88 @@
 
             <!-- Layout container -->
             <div class="layout-page">
-                <!-- Navbar -->
-                <nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
-                    id="layout-navbar">
-                    <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
-                        <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
-                            <i class="bx bx-menu bx-sm"></i>
-                        </a>
-                    </div>
-
-                    <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
-                        <!-- Search -->
-                        <div class="navbar-nav align-items-center">
-                            <div class="nav-item d-flex align-items-center">
-                                <i class="bx bx-search fs-4 lh-0"></i>
-                                <input type="text" class="form-control border-0 shadow-none" placeholder="Search..."
-                                    aria-label="Search..." />
-                            </div>
-                        </div>
-                        <!-- /Search -->
-                    </div>
-                </nav>
-                <!-- / Navbar -->
+                <x-dashboard.navbar />
 
                 {{-- Content --}}
                 <div class="content-wrapper">
-                    <!-- Content -->
-                    <div class="card mb-4">
-                        <h5 class="card-header">Branch management</h5>
-                        <!-- Account -->
-                        <div class="card-body">
-                            <div class="d-flex align-items-start align-items-sm-center gap-4">
-                                <img src="../assets/img/avatars/1.png" alt="user-avatar" class="d-block rounded"
-                                    height="100" width="100" id="uploadedAvatar" />
-                                <div class="button-wrapper">
-                                    <label for="upload" class="btn btn-primary me-2 mb-4" tabindex="0">
-                                        <span class="d-none d-sm-block">Upload new photo</span>
-                                        <i class="bx bx-upload d-block d-sm-none"></i>
-                                        <input type="file" id="upload" class="account-file-input" hidden
-                                            accept="image/png, image/jpeg" />
-                                    </label>
-                                    <button type="button" class="btn btn-outline-secondary account-image-reset mb-4">
-                                        <i class="bx bx-reset d-block d-sm-none"></i>
-                                        <span class="d-none d-sm-block">Reset</span>
-                                    </button>
+                    <div class="container-xxl flex-grow-1 container-p-y">
+                        <!-- Content -->
+                        <div class="card mb-4">
+                            <h5 class="card-header">Branch management</h5>
+                            <!-- Account -->
+                            <div class="card-body">
+                                <div class="d-flex align-items-start align-items-sm-center gap-4">
+                                    <img src="../assets/img/avatars/1.png" alt="user-avatar" class="d-block rounded"
+                                        height="100" width="100" id="uploadedAvatar" />
+                                    <div class="button-wrapper">
+                                        <label for="upload" class="btn btn-primary me-2 mb-4" tabindex="0">
+                                            <span class="d-none d-sm-block">Upload new photo</span>
+                                            <i class="bx bx-upload d-block d-sm-none"></i>
+                                            <input type="file" id="upload" class="account-file-input" hidden
+                                                accept="image/png, image/jpeg" />
+                                        </label>
+                                        <button type="button"
+                                            class="btn btn-outline-secondary account-image-reset mb-4">
+                                            <i class="bx bx-reset d-block d-sm-none"></i>
+                                            <span class="d-none d-sm-block">Reset</span>
+                                        </button>
 
-                                    <p class="text-muted mb-0">Allowed JPG, GIF or PNG. Max size of 800K</p>
+                                        <p class="text-muted mb-0">Allowed JPG, GIF or PNG. Max size of 800K</p>
+                                    </div>
+                                </div>
+                                <div class="d-flex align-items-start align-items-sm-center gap-4">
+                                    <img src="../assets/img/avatars/1.png" alt="user-avatar" class="d-block rounded"
+                                        height="100" width="100" id="uploadedAvatar" />
+                                    <div class="button-wrapper">
+                                        <label for="upload" class="btn btn-primary me-2 mb-4" tabindex="0">
+                                            <span class="d-none d-sm-block">Upload new photo</span>
+                                            <i class="bx bx-upload d-block d-sm-none"></i>
+                                            <input type="file" id="upload" class="account-file-input" hidden
+                                                accept="image/png, image/jpeg" />
+                                        </label>
+                                        <button type="button"
+                                            class="btn btn-outline-secondary account-image-reset mb-4">
+                                            <i class="bx bx-reset d-block d-sm-none"></i>
+                                            <span class="d-none d-sm-block">Reset</span>
+                                        </button>
+
+                                        <p class="text-muted mb-0">Allowed JPG, GIF or PNG. Max size of 800K</p>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="d-flex align-items-start align-items-sm-center gap-4">
-                                <img src="../assets/img/avatars/1.png" alt="user-avatar" class="d-block rounded"
-                                    height="100" width="100" id="uploadedAvatar" />
-                                <div class="button-wrapper">
-                                    <label for="upload" class="btn btn-primary me-2 mb-4" tabindex="0">
-                                        <span class="d-none d-sm-block">Upload new photo</span>
-                                        <i class="bx bx-upload d-block d-sm-none"></i>
-                                        <input type="file" id="upload" class="account-file-input" hidden
-                                            accept="image/png, image/jpeg" />
-                                    </label>
-                                    <button type="button" class="btn btn-outline-secondary account-image-reset mb-4">
-                                        <i class="bx bx-reset d-block d-sm-none"></i>
-                                        <span class="d-none d-sm-block">Reset</span>
-                                    </button>
 
-                                    <p class="text-muted mb-0">Allowed JPG, GIF or PNG. Max size of 800K</p>
-                                </div>
-                            </div>
-                        </div>
-                        <hr class="my-0" />
-                        <div class="card-body">
-                            <form id="formAccountSettings" method="POST" onsubmit="return false">
-                                <div class="row">
+                            <hr class="my-0" />
+
+                            <div class="card-body">
+                                <form id="formAccountSettings" method="POST" onsubmit="return false">
                                     <div class="mb-3 col-md-6">
                                         <label for="firstName" class="form-label">Nama Perusahaan</label>
                                         <input class="form-control" placeholder="Nama Perusahaan" type="text"
                                             id="firstName" name="firstName" autofocus autocomplete="off" />
                                     </div>
 
-                                </div>
-                                <div class="mb-3 col-md-6">
-                                    <label class="form-label" for="phoneNumber">Phone Number</label>
-                                    <div class="input-group input-group-merge">
-                                        <span class="input-group-text">INA (+62)</span>
-                                        <input type="text" id="phoneNumber" name="phoneNumber" class="form-control"
-                                            placeholder="8XXX XXXX XXX" />
+                                    <div class="mb-3 col-md-6">
+                                        <label class="form-label" for="phoneNumber">Phone Number</label>
+                                        <div class="input-group input-group-merge">
+                                            <span class="input-group-text">INA (+62)</span>
+                                            <input type="text" id="phoneNumber" name="phoneNumber"
+                                                class="form-control" placeholder="8XXX XXXX XXX" />
+                                        </div>
                                     </div>
-                                </div>
 
-                                <div class="mb-3 col-md-6">
-                                    <label for="state" class="form-label">alamat</label>
-                                    <input class="form-control" type="text" id="state" name="state"
-                                        placeholder="Surabaya" />
-                                </div>
+                                    <div class="mb-3 col-md-6">
+                                        <label for="state" class="form-label">alamat</label>
+                                        <input class="form-control" type="text" id="state" name="state"
+                                            placeholder="Surabaya" autocomplete="off" />
+                                    </div>
 
-                                <div class="mt-2">
-                                    <button type="submit" class="btn btn-primary me-2">Save
-                                        changes</button>
-                                    <button type="reset" class="btn btn-outline-secondary">Cancel</button>
-                                </div>
-                            </form>
+                                    <div class="mt-2">
+                                        <button type="submit" class="btn btn-primary me-2">Save
+                                            changes</button>
+                                        <button type="reset" class="btn btn-outline-secondary">Cancel</button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
--- resources/views/dashboard/products.blade.php
+++ resources/views/dashboard/products.blade.php
@@ -1,3 +1,97 @@
 <x-dashboard-layout>
-    <p class="">products management page</p>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+        <div class="layout-container">
+
+            <x-dashboard.sidebar />
+
+            <!-- Layout container -->
+            <div class="layout-page">
+                <x-dashboard.navbar />
+
+                {{-- Content --}}
+                <div class="content-wrapper">
+
+                    <!-- Content -->
+                    <div class="container-xxl flex-grow-1 container-p-y">
+                        <h4 class="fw-bold py-3 mb-4">
+                            Add Product
+                        </h4>
+
+                        <div class="row">
+                            <div class="col-md-12">
+
+                                <div class="card mb-4">
+                                    <h5 class="card-header">Product Details</h5>
+                                    <!-- Product -->
+                                    <div class="card-body">
+                                        <div class="d-flex align-items-start align-items-sm-center gap-4">
+                                            <img src={{ asset('/img/avatars/1.png') }} alt="user-avatar"
+                                                class="d-block rounded" height="100" width="100"
+                                                id="uploadedAvatar" />
+                                            <div class="button-wrapper">
+                                                <label for="upload" class="btn btn-primary me-2 mb-4" tabindex="0">
+                                                    <span class="d-none d-sm-block">Upload new image</span>
+                                                    <i class="bx bx-upload d-block d-sm-none"></i>
+                                                    <input type="file" id="upload" class="account-file-input"
+                                                        hidden accept="image/png, image/jpeg" />
+                                                </label>
+                                                <button type="button"
+                                                    class="btn btn-outline-secondary account-image-reset mb-4">
+                                                    <i class="bx bx-reset d-block d-sm-none"></i>
+                                                    <span class="d-none d-sm-block">Reset</span>
+                                                </button>
+
+                                                <p class="text-muted mb-0">Allowed JPG, GIF or PNG. Max size of 800K</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <hr class="my-0" />
+                                    <div class="card-body">
+                                        <form id="formAccountSettings" method="POST" onsubmit="return false">
+                                            <div class="row">
+                                                <div class="mb-3 col-md-6">
+                                                    <label for="product" class="form-label">Product Name</label>
+                                                    <input class="form-control" placeholder="Product" autocomplete="off"
+                                                        type="text" id="product" name="product" autofocus />
+                                                </div>
+                                                <div class="mb-3 col-md-6">
+                                                    <label for="price" class="form-label">Price</label>
+                                                    <input class="form-control" placeholder="Price" autocomplete="off"
+                                                        type="text" id="price" name="price" autofocus />
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="basic-default-message">
+                                                        Product Description
+                                                    </label>
+                                                    <textarea id="basic-default-message" name="description" class="form-control"
+                                                        placeholder="Hi, Do you have a moment to talk Joe?"></textarea>
+                                                </div>
+
+                                                <div class="mt-2">
+                                                    <button type="submit" class="btn btn-primary me-2">Save
+                                                        changes</button>
+                                                    <button type="reset"
+                                                        class="btn btn-outline-secondary">Cancel</button>
+                                                </div>
+                                        </form>
+                                    </div>
+                                    <!-- /Product -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- / Content -->
+
+                    <div class="content-backdrop fade"></div>
+                </div>
+                {{-- End Content --}}
+            </div>
+            <!-- / Layout page -->
+        </div>
+
+        <!-- Overlay -->
+        <div class="layout-overlay layout-menu-toggle"></div>
+    </div>
+    <!-- / Layout wrapper -->
 </x-dashboard-layout>
