967b8d75b6f146f77be4d4d772e78bb569fc4a0e
zuhalcode <zuhalcode@gmail.com>
2023-04-14 15:08:54 +0700

    fixing assets

--- resources/views/components/layouts/dashboard-layout.blade.php
+++ resources/views/components/layouts/dashboard-layout.blade.php
@@ -21,24 +21,24 @@
         rel="stylesheet" />
 
     <!-- Icons. Uncomment required icon fonts -->
-    <link rel="stylesheet" href={{ asset('vendor/fonts/boxicons.css') }} />
+    <link rel="stylesheet" href={{ asset('/vendor/fonts/boxicons.css') }} />
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href={{ asset('vendor/css/core.css') }} class="template-customizer-core-css" />
-    <link rel="stylesheet" href={{ asset('vendor/css/theme-default.css') }} class="template-customizer-theme-css" />
-    <link rel="stylesheet" href={{ asset('css/demo.css') }} />
+    <link rel="stylesheet" href={{ asset('/vendor/css/core.css') }} class="template-customizer-core-css" />
+    <link rel="stylesheet" href={{ asset('/vendor/css/theme-default.css') }} class="template-customizer-theme-css" />
+    <link rel="stylesheet" href={{ asset('/css/demo.css') }} />
 
     <!-- Vendors CSS -->
-    <link rel="stylesheet" href={{ asset('vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }} />
-    <link rel="stylesheet" href={{ asset('vendor/libs/apex-charts/apex-charts.css') }} />
+    <link rel="stylesheet" href={{ asset('/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }} />
+    <link rel="stylesheet" href={{ asset('/vendor/libs/apex-charts/apex-charts.css') }} />
     <!-- Page CSS -->
 
     <!-- Helpers -->
-    <script src={{ asset('vendor/js/helpers.js') }}></script>
+    <script src={{ asset('/vendor/js/helpers.js') }}></script>
 
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src={{ asset('js/config.js') }}></script>
+    <script src={{ asset('/js/config.js') }}></script>
 </head>
 
 <body>
@@ -46,23 +46,23 @@
 
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
-    <script src="{{ asset('vendor/libs/jquery/jquery.js') }}"></script>
-    <script src="{{ asset('vendor/libs/popper/popper.js') }}"></script>
-    <script src="{{ asset('vendor/js/bootstrap.js') }}"></script>
-    <script src="{{ asset('vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('/vendor/libs/jquery/jquery.js') }}"></script>
+    <script src="{{ asset('/vendor/libs/popper/popper.js') }}"></script>
+    <script src="{{ asset('/vendor/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
 
-    <script src="{{ asset('vendor/js/menu.js') }}"></script>
+    <script src="{{ asset('/vendor/js/menu.js') }}"></script>
     <!-- endbuild -->
 
     <!-- Vendors JS -->
-    <script src="{{ asset('vendor/libs/apex-charts/apexcharts.js') }}"></script>
+    <script src="{{ asset('/vendor/libs/apex-charts/apexcharts.js') }}"></script>
 
     <!-- Main JS -->
-    <script src="{{ asset('js/main.js') }}"></script>
-    <script src="{{ asset('js/script.js') }}"></script>
+    <script src="{{ asset('/js/main.js') }}"></script>
+    <script src="{{ asset('/js/script.js') }}"></script>
 
     <!-- Page JS -->
-    <script src="{{ asset('js/dashboards-analytics.js') }}"></script>
+    <script src="{{ asset('/js/dashboards-analytics.js') }}"></script>
 
     <!-- Place this tag in your head or just before your close body tag. -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
