28e94563da69326e4de9fc1705f440a700feeef1
erlanggad <erlangga.20038@mhs.unesa.ac.id>
2023-04-17 12:42:20 +0700

    menambahkan navbar pada product detail

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,4 +1,259 @@
 <x-layouts.app-layout>
+
+    <nav class="navigation">
+        <div class="nav-section">
+            <div class="header-section">
+                <div class="navbar navbar-expand-xl navbar-light navbar-sticky p-0">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#primaryMenu"
+                        aria-controls="primaryMenu">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <a href="index.html" class="logo-link">
+                        <img class="logo logo-dark" src="../assets/images/logos/logo.png" alt="logo" />
+                        <img class="logo logo-light" src="../assets/images/logos/logo-w.png" alt="logo" />
+                    </a>
+                    <div class="offcanvas offcanvas-collapse order-lg-2" id="primaryMenu">
+                        <div class="offcanvas-header navbar-shadow">
+                            <h5 class="mt-1 mb-0">Menu</h5>
+                            <button class="btn-close lead" type="button" data-bs-dismiss="offcanvas"
+                                aria-label="Close"></button>
+                        </div>
+
+                        <div class="offcanvas-body">
+                            <!-- Menu-->
+                            <ul class="navbar-nav">
+                                <!-- Home -->
+                                <li class="nav-item dropdown dropdown-mega">
+                                    <a class="nav-link dropdown-toggle" href="#"
+                                        data-bs-toggle="dropdown">Home</a>
+                                    <div class="dropdown-menu home-menu">
+                                        <div class="row g-4">
+                                            <div class="col-xl-2 mega-box">
+                                                <a class="home-box" href="index.html">
+                                                    <img class="img-fluid" src="../assets/images/demo-img/demo1.jpg"
+                                                        alt="home1" />
+                                                    <span>Fashion Demo</span>
+                                                </a>
+                                            </div>
+
+                                            <div class="col-xl-2 mega-box">
+                                                <a class="home-box" href="index-2.html">
+                                                    <img class="img-fluid" src="../assets/images/demo-img/demo2.jpg"
+                                                        alt="home2" />
+                                                    <span>Bag Demo</span>
+                                                </a>
+                                            </div>
+
+                                            <div class="col-xl-2 mega-box">
+                                                <a class="home-box" href="index-3.html">
+                                                    <img class="img-fluid" src="../assets/images/demo-img/demo3.jpg"
+                                                        alt="home3" />
+                                                    <span>Grocery Demo</span>
+                                                </a>
+                                            </div>
+
+                                            <div class="col-xl-2 mega-box">
+                                                <a class="home-box" href="index-4.html">
+                                                    <img class="img-fluid" src="../assets/images/demo-img/demo4.jpg"
+                                                        alt="home4" />
+                                                    <span>Furniture Demo</span>
+                                                </a>
+                                            </div>
+
+                                            <div class="col-xl-2 mega-box">
+                                                <a class="home-box" href="index-5.html">
+                                                    <img class="img-fluid" src="../assets/images/demo-img/demo5.jpg"
+                                                        alt="home5" />
+                                                    <span>Watch Demo</span>
+                                                </a>
+                                            </div>
+
+                                            <div class="col-xl-2 mega-box">
+                                                <a class="home-box" href="index-6.html">
+                                                    <img class="img-fluid" src="../assets/images/demo-img/demo6.jpg"
+                                                        alt="home6" />
+                                                    <span>Shoes Demo</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <!-- Shop -->
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#"
+                                        data-bs-toggle="dropdown">Shop</a>
+                                    <div class="dropdown-menu">
+                                        <div class="dropdown-column">
+                                            <a class="dropdown-item" href="shop-categories.html">Shop Categories</a>
+                                            <a class="dropdown-item" href="shop-filter.html">Shop Filter</a>
+                                            <a class="dropdown-item" href="shop-left-sidebar.html">Shop Left Sidebar</a>
+                                            <a class="dropdown-item" href="shop-list.html">Shop List</a>
+                                            <a class="dropdown-item" href="shop-masanory.html">Shop Masonry</a>
+                                            <a class="dropdown-item" href="shop-no-sidebar.html">Shop No Sidebar</a>
+                                            <a class="dropdown-item" href="shop-right-sidebar.html">Shop Right
+                                                Sidebar</a>
+                                            <a class="dropdown-item" href="shop-top-filter.html">Shop Top Filter</a>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <!-- Product -->
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#"
+                                        data-bs-toggle="dropdown">Product</a>
+                                    <div class="dropdown-menu">
+                                        <div class="dropdown-column">
+                                            <a class="dropdown-item" href="product-four-image.html">Product 4 Images</a>
+                                            <a class="dropdown-item" href="product-slider.html">Product Slider </a>
+                                            <a class="dropdown-item" href="product-sticky.html">Product Sticky</a>
+                                            <a class="dropdown-item" href="product-three-column.html">Product 3
+                                                Column</a>
+                                            <a class="dropdown-item" href="product-video-img.html">Product Video</a>
+                                            <a class="dropdown-item" href="product.html">Product Simple</a>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <!-- Oslo Plus -->
+                                <li class="nav-item dropdown dropdown-mega">
+                                    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Oslo
+                                        Plus</a>
+                                    <div class="dropdown-menu mega-element-menu">
+                                        <div class="row g-4">
+                                            <div class="col-xl-3">
+                                                <div class="link-box">
+                                                    <h5>Portfolio Pages</h5>
+                                                    <div class="dropdown-column">
+                                                        <a class="dropdown-item"
+                                                            href="portfolio-2-grid.html">Portfolio 2 Grid</a>
+                                                        <a class="dropdown-item"
+                                                            href="portfolio-2-grid-masonry.html">Portfolio 2 Grid
+                                                            Masonry</a>
+                                                        <a class="dropdown-item"
+                                                            href="portfolio-3-grid.html">Portfolio 3 Grid <span
+                                                                class="badge bg-theme-theme">Hot</span></a>
+                                                        <a class="dropdown-item"
+                                                            href="portfolio-3-grid-masonry.html">Portfolio 3 Grid
+                                                            Masonry</a>
+                                                        <a class="dropdown-item"
+                                                            href="portfolio-4-grid.html">Portfolio 4 Grid</a>
+                                                        <a class="dropdown-item"
+                                                            href="portfolio-4-grid-masonry.html">Portfolio 4 Grid
+                                                            Masonry</a>
+                                                        <a class="dropdown-item"
+                                                            href="portfolio-no-space-masonry.html">Portfolio No Space
+                                                            Masonry</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-xl-3">
+                                                <div class="link-box">
+                                                    <h5>Elements Pages</h5>
+                                                    <div class="dropdown-column">
+                                                        <a class="dropdown-item" href="banner-element.html">Banner
+                                                            Element</a>
+                                                        <a class="dropdown-item" href="brand-element.html">Brand
+                                                            Element</a>
+                                                        <a class="dropdown-item" href="button-element.html">Button
+                                                            Element </a>
+                                                        <a class="dropdown-item"
+                                                            href="categories-element.html">Categories Element</a>
+                                                        <a class="dropdown-item"
+                                                            href="collection-banner-element.html">Collection Banner
+                                                            Element</a>
+                                                        <a class="dropdown-item" href="home-element.html">Home Banner
+                                                            Element</a>
+                                                        <a class="dropdown-item"
+                                                            href="instagram-element.html">Instagram Element <span
+                                                                class="badge bg-theme-sun2">New</span></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-xl-3">
+                                                <div class="link-box">
+                                                    <h5>Elements Pages</h5>
+                                                    <div class="dropdown-column">
+                                                        <a class="dropdown-item"
+                                                            href="offer-banner-element.html">Offer Banner Element</a>
+                                                        <a class="dropdown-item" href="brand-element.html">Brand
+                                                            Element <span class="badge bg-theme-theme">Hot</span></a>
+                                                        <a class="dropdown-item"
+                                                            href="product-card-element.html">Product Card Element</a>
+                                                        <a class="dropdown-item" href="seller-element.html">Seller
+                                                            Section Element</a>
+                                                        <a class="dropdown-item" href="service-element.html">Service
+                                                            Section Element</a>
+                                                        <a class="dropdown-item" href="tabs-element.html">Tabs
+                                                            Element</a>
+                                                        <a class="dropdown-item" href="title-element.html">Title
+                                                            Element</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-xl-3 d-none d-xl-block">
+                                                <div class="link-box">
+                                                    <div class="menu-img">
+                                                        <img class="img-fluid"
+                                                            src="../assets/images/demo-img/menu-bg.jpg"
+                                                            alt="img" />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <!-- Pages -->
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#"
+                                        data-bs-toggle="dropdown">Pages</a>
+                                    <div class="dropdown-menu">
+                                        <div class="dropdown-column">
+                                            <a class="dropdown-item" href="404.html">404</a>
+                                            <a class="dropdown-item" href="about-us.html">About Us </a>
+                                            <a class="dropdown-item" href="address.html">Address</a>
+                                            <a class="dropdown-item" href="cart.html">Cart</a>
+                                            <a class="dropdown-item" href="coming-soon.html">Coming Soon</a>
+                                            <a class="dropdown-item" href="compare.html">Compare</a>
+                                            <a class="dropdown-item" href="contact-us.html">Contact Us</a>
+                                            <a class="dropdown-item" href="faqs.html">Faqs</a>
+                                            <a class="dropdown-item" href="forgot-password.html">Forgot Password</a>
+                                            <a class="dropdown-item" href="login.html">Login</a>
+                                            <a class="dropdown-item" href="order-success.html">Order Success</a>
+                                            <a class="dropdown-item" href="otp.html">Otp</a>
+                                            <a class="dropdown-item" href="payment.html">Payment</a>
+                                            <a class="dropdown-item" href="register.html">Register</a>
+                                            <a class="dropdown-item" href="reset-password.html">Reset Password</a>
+                                            <a class="dropdown-item" href="search.html">Search</a>
+                                            <a class="dropdown-item" href="user-dashboard.html">User Dashboard</a>
+                                            <a class="dropdown-item" href="wishlist.html">Wishlist</a>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <!-- Blog -->
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#"
+                                        data-bs-toggle="dropdown">Blog</a>
+                                    <div class="dropdown-menu">
+                                        <div class="dropdown-column">
+                                            <a class="dropdown-item" href="blog-detail.html">Blog Details</a>
+                                            <a class="dropdown-item" href="blog-grid.html">Blog Grid </a>
+                                            <a class="dropdown-item" href="blog-list.html">Blog List</a>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
     <!-- Product Section Start -->
     <section class="product-page">
         <div class="container-lg">
